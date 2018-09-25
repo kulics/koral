6ab26a87f294acfa980fba3cd5adec53e32ad21a
kulics <kulics@naxy.me>
2018-09-25 21:55:50 +0800

    update lib;

---
+++ Library/Shared/CommandLine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace XyLang.Library
+{
+    public static class cmd
+    {
+        public static void print(params object[] paramList)
+        {
+            foreach (var item in paramList)
+            {
+                Console.Write(item);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+
+        public static str read() => Console.ReadLine();
+
+        public static void clear() => Console.Clear();
+    }
+}
--- Library/Shared/Tasks.cs
+++ Library/Shared/Tasks.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -18,6 +19,40 @@ namespace XyLang.Library
             : base(action, state, creationOptions) { }
         public tsk(Action<object> action, object state, CancellationToken cancellationToken, TaskCreationOptions creationOptions)
             : base(action, state, cancellationToken, creationOptions) { }
+
+        public bool isCompleted => IsCompleted;
+        public int id => Id;
+        public bool isCanceled => IsCanceled;
+        public object asyncState => AsyncState;
+        public bool isFaulted => IsFaulted;
+        public TaskStatus status => Status;
+        public void start() => Start();
+        public void start(TaskScheduler scheduler) => Start(scheduler);
+        public bool wait(i32 millisecondsTimeout) => Wait(millisecondsTimeout);
+        public bool wait(i32 millisecondsTimeout, CancellationToken cancellationToken) => Wait(millisecondsTimeout, cancellationToken);
+        public void wait() => Wait();
+    }
+
+    public static class tsks
+    {
+        public static Task run(Func<Task> function, CancellationToken cancellationToken) => Task.Run(function, cancellationToken);
+        public static Task run(Action action, CancellationToken cancellationToken) => Task.Run(action, cancellationToken);
+        public static Task run(Action action) => Task.Run(action);
+        public static Task run(Func<Task> function) => Task.Run(function);
+
+        public static void waitAll(Task[] tasks, CancellationToken cancellationToken) => Task.WaitAll(tasks, cancellationToken);
+        public static bool waitAll(Task[] tasks, int millisecondsTimeout, CancellationToken cancellationToken) => Task.WaitAll(tasks, millisecondsTimeout, cancellationToken);
+        public static bool waitAll(Task[] tasks, int millisecondsTimeout) => Task.WaitAll(tasks, millisecondsTimeout);
+        public static void waitAll(params Task[] tasks) => Task.WaitAll(tasks);
+
+        public static Task whenAll(IEnumerable<Task> tasks) => Task.WhenAll(tasks);
+        public static Task whenAll(params Task[] tasks) => Task.WhenAll(tasks);
+
+        public static Task<Task> whenAny(IEnumerable<Task> tasks) => Task.WhenAny(tasks);
+        public static Task<Task> whenAny(params Task[] tasks) => Task.WhenAny(tasks);
+
+        public static Task delay(i32 millisecondsDelay) => Task.Delay(millisecondsDelay);
+        public static Task delay(i32 millisecondsDelay, CancellationToken cancellationToken) => Task.Delay(millisecondsDelay, cancellationToken);
     }
 
     public class tsk<TResult> : Task<TResult>
@@ -34,5 +69,17 @@ namespace XyLang.Library
             : base(function, state, creationOptions) { }
         public tsk(Func<object, TResult> function, object state, CancellationToken cancellationToken, TaskCreationOptions creationOptions)
             : base(function, state, cancellationToken, creationOptions) { }
+
+        public bool isCompleted => IsCompleted;
+        public int id => Id;
+        public bool isCanceled => IsCanceled;
+        public object asyncState => AsyncState;
+        public bool isFaulted => IsFaulted;
+        public TaskStatus status => Status;
+        public void start() => Start();
+        public void start(TaskScheduler scheduler) => Start(scheduler);
+        public bool wait(i32 millisecondsTimeout) => Wait(millisecondsTimeout);
+        public bool wait(i32 millisecondsTimeout, CancellationToken cancellationToken) => Wait(millisecondsTimeout, cancellationToken);
+        public void wait() => Wait();
     }
 }
