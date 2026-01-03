f667a6c8b6fba0e38eda2ce1232a1ae013750f05
kulics <kulics@outlook.com>
2026-01-03 19:45:37 +0800

    adapted windows11

--- compiler/Tests/koralcTests/koralcTests.swift
+++ compiler/Tests/koralcTests/koralcTests.swift
@@ -1,6 +1,6 @@
 import Testing
 
-@testable import koralc
+@testable import KoralCompiler
 
 @Test func example() async throws {
   // Write your test here and use APIs like `#expect(...)` to check expected conditions.
